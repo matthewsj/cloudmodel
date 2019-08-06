66771387b5400502ed9aeb6f771c6c8621e329a9
Yonatan Kogan <yjkogan@gmail.com>
2019-08-06 11:23:18 -0400

    Remove whitespace

--- examples/chatapp/src/client/index.js
+++ examples/chatapp/src/client/index.js
@@ -3,7 +3,6 @@ import './index.html';
 import { Elm } from './elm/Main.elm';
 import { initializePorts } from 'elm-cloudmodel';
 
-
 const app = Elm.Main.init({
   node: document.getElementById('elm')
 });
